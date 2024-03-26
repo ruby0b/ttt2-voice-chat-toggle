9228f8e8b898ff88aa0bf44d2d6f3f2dd4bed13f
ruby0b <106119328+ruby0b@users.noreply.github.com>
2024-03-26 15:54:17 +0100

    remove panel hiding (moved to different addon)

--- lua/terrortown/menus/gamemode/server_addons/voice_chat_toggle.lua
+++ lua/terrortown/menus/gamemode/server_addons/voice_chat_toggle.lua
@@ -10,15 +10,4 @@ function CLGAMEMODESUBMENU:Populate(parent)
         serverConvar = "voice_toggle_auto_enable",
         label = "Automatically enable voice chat for players when they join."
     })
-
-    local hide_panels = form:MakeCheckBox({
-        serverConvar = "voice_toggle_hide_panels",
-        label = "Hide the voice panels that show who else is talking."
-    })
-
-    form:MakeCheckBox({
-        serverConvar = "voice_toggle_show_panels_spectator",
-        label = "Show voice panels for fellow dead players while spectating.",
-        master = hide_panels
-    })
 end
